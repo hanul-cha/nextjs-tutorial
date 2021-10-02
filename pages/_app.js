73a754e00b7972f4ec8b3bcdbff279fee157f67a
hanul-cha <ckgks3014@naver.com>
2021-10-02 13:07:29 +0900

    git

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+/* 
+페이지 전환시 레이아웃, 상태값을 유지할수 있음
+componentDidCatch를 이용하여 커스텀 에러 헨들링을 할수 있음
+추가적인 테이터를 페이지로 주입시켜줄수 있음
+글로벌css를 이곳에 import해줌
+*/
