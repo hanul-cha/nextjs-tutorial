58bb55071832b6ce1f82cb1456bbfd45a82f45aa
hanul-cha <ckgks3014@naver.com>
2021-10-03 14:48:26 +0900

    액시오스를 이용한 api data가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
+
+import axios from 'axios';
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [list, setList] = useState([]);
+
+  const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
+  function getDate() {
+    axios.get(API_URL)
+      .then(res => {
+        console.log(res.data)
+        setList(res.data)
+      });
+  }
+
+  useEffect(() => {
+    getDate();
+  }, []);
+
   return (
     <div>
       <Head>
