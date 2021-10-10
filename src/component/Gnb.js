b52b1160d1760b7392237a60e9ed389fcebf694b
hanul-cha <ckgks3014@naver.com>
2021-10-10 13:03:58 +0900

    로그인 페이지 ui

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -10,6 +10,8 @@ export default function Gnb() {
         activeItem = "home";
     } else if(router.pathname === "/about"){
         activeItem = "about";
+    } else if(router.pathname === "/admin"){
+        activeItem = "admin";
     }
 
 
@@ -38,6 +40,13 @@ export default function Gnb() {
             active={activeItem === 'contact'}
             onClick={() => {
                 router.push("/contact");
+            }}
+            />
+            <Menu.Item
+            name='admin'
+            active={activeItem === 'admin'}
+            onClick={() => {
+                router.push("/admin");
             }}
             />
             
